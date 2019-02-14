aaa0218dc6f272f0723d80ab1747fe828e1fea30
mexskican <benoit.verkindt@gmail.com>
2019-02-14 15:50:12 -0800

    Publish to RabbitMQ exchange

--- rpc/src/v1/rabbitmq/interface.rs
+++ rpc/src/v1/rabbitmq/interface.rs
@@ -1,26 +1,10 @@
-// Copyright 2015-2018 Parity Technologies (UK) Ltd.
-// This file is part of Parity.
-
-// Parity is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// Parity is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with Parity.  If not, see <http://www.gnu.org/licenses/>.
-
-//! RabbitMQ ChainNotfiy implementation
+//! RabbitMQ broker interface
 
 use std::net::SocketAddr;
 
 use failure::Error;
 use futures::future::Future;
-use lapin::channel::{BasicPublishOptions, BasicProperties, QueueDeclareOptions};
+use lapin::channel::{BasicPublishOptions, BasicProperties, ExchangeDeclareOptions};
 use lapin::client::ConnectionOptions;
 use lapin::types::FieldTable;
 use tokio::net::TcpStream;
@@ -45,7 +29,7 @@ impl RabbitMqInterface {
 	}
 
 	/// Publish a new message to the defined queue
-	pub fn publish(&self, serialized_block: String, queue_name: &'static str) {
+	pub fn publish(&self, serialized_data: String, exchange_name: &'static str) {
 		let url = format!("{}:{}", self.config.hostname, self.config.port).parse().unwrap();
 		Runtime::new().unwrap().block_on_all(
 			TcpStream::connect(&url).map_err(Error::from).and_then(|stream| {
@@ -53,8 +37,8 @@ impl RabbitMqInterface {
 			}).and_then(|(client, _ /* heartbeat */)| {
 				client.create_channel().map_err(Error::from)
 			}).and_then(move |channel| {
-				channel.queue_declare(queue_name, QueueDeclareOptions::default(), FieldTable::new()).and_then(move |_| {
-					channel.basic_publish("", queue_name, serialized_block.into_bytes(), BasicPublishOptions::default(), BasicProperties::default())
+				channel.exchange_declare(exchange_name, "direct", ExchangeDeclareOptions::default(), FieldTable::new()).and_then(move |_| {
+					channel.basic_publish(exchange_name, "parity", serialized_data.into_bytes(), BasicPublishOptions::default(), BasicProperties::default())
 				}).map_err(Error::from)
 			})
 		).expect("Runtime failure");
