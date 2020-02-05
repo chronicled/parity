8ed1a63c37b124cb8830bcc5d694fe399a2b5f2f
Jack Lee <jack.y.l.dev@gmail.com>
2020-02-04 17:24:45 -0800

    remove commented lines

--- rabbitmq/src/client.rs
+++ rabbitmq/src/client.rs
@@ -315,8 +315,6 @@ fn publish_new_block(
 			info!(target: LOG_TARGET, "Error publishing: {}", err);
 			handle_fatal_error(err);
 		})
-		// .timeout(Duration::from_secs(10))
-		// .map_err(|_| ())
 		.map(move |_| {
 			info!(target: LOG_TARGET, "Block message published: {:?}", block_number);
 			()
